5d9cb24ad3d3b8fcf0df5a4620531a00f6f6b0a2
Lucas <lucasmartinsde@gmail.com>
2022-02-24 17:02:34 -0300

    24-02-2022

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings , WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -19,6 +20,14 @@ class TarefasExport implements FromCollection, WithHeadings
     }
 
     public function headings():array{ //declarando o tipo de retorno (array)
-        return ['ID Da Tarefa','ID do Usuario','Tarefa','Data de Limite de Conclusao','Data Criacao','Data Atualizacao'];
+        return ['ID Da Tarefa','Tarefa','Data de Limite de Conclusao'];
+    }
+
+    public function map($linha):array { //linha a linha
+        return [
+            $linha->id,
+            $linha->tarefa,
+            date('d/m/Y',strtotime($linha->data_limite_conclusao))
+        ];
     }
 }
